261ed9db1e3a630ba4d3b7583d968825744178b7
Hilal38 <hhilalkocak@gmail.com>
2024-03-11 06:49:56 +0300

    Add BFS fn

--- graphalg.js
+++ graphalg.js
@@ -15,6 +15,18 @@ const deptFirstPrint = (graph, source) => {
         deptFirstPrint(graph, neigbor);
     }
 }
+
+const breadthFirstPrint = (graph, source) => {
+    const queue = [source];
+    while(queue.length > 0){
+        const current = queue.shift();
+        console.log(current)
+        for(let neighbor of graph[current]){
+            queue.push(neighbor)
+        }
+    }
+    
+}
 const graph = {
     a: ['c', 'b'],
     b: ['d'],
@@ -23,5 +35,7 @@ const graph = {
     e: [],
     f: []
 }
-
+console.log("-- DFS --")
 deptFirstPrint(graph, 'a'); // abdfce and acebdf both valid
+console.log("-- BFS --")
+breadthFirstPrint(graph, 'a') // acbedf
