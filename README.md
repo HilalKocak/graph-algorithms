ad55f52f002e9f0fcc96a1b005243a2bbbed3e90
Hilal38 <hhilalkocak@gmail.com>
2024-03-11 12:15:48 +0300

    Implement hasPath problem for undirected graphs

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 ### Graph Algorithms
+* Directed graphs
+* Traverse a graph using
+- Deep-First Search and Breadth-First Search
+* hasPath Problem: Is there a way to determine if there's a path from one node to another with  dept-first stack and breadth-first queue ın directed graphs and undirected graphs
 
 - To clone this project, run:
 ```
--- graphalg.js
+++ graphalg.js
@@ -51,30 +51,59 @@ const graphForHasPathProblem = {
     'k': []
 }
 // hasPath Problem = Depth First Stack
-// const hasPath = (graph, src, dst) => {
-//     if(src === dst) return true;
-//     for(let neigbor of graph[src]){
-//         if(hasPath(graph, neigbor, dst) === true){
-//             return true
-//         }
-//     }
-
-//     return false
-// }
-
-// hasPath Problem = Breadth First Stack 
-const hasPath = (graph, src, dst) => {
-    const queue = [src]
-
-    while (queue.length > 0) {
-        const current = queue.shift();
-        if(current === dst) return true;
-        for(let neighbor of graph[current]){
-            queue.push(neighbor)
+const hasPath = (graph, src, dst, visited = new Set()) => {
+    if(visited.has(src)) return false;
+    visited.add(src)
+    
+    if(src === dst) return true;
+    for(let neighbor of graph[src]){
+        if(hasPath(graph, neighbor, dst, visited) === true){
+            return true
         }
     }
 
     return false
 }
 
+// hasPath Problem = Breadth First Stack 
+// const hasPath = (graph, src, dst) => {
+//     const queue = [src]
+
+//     while (queue.length > 0) {
+//         const current = queue.shift();
+//         if(current === dst) return true;
+//         for(let neighbor of graph[current]){
+//             queue.push(neighbor)
+//         }
+//     }
+
+//     return false
+// }
+
 console.log('f to k hasPath? ', hasPath(graphForHasPathProblem, 'f', 'k'))
+
+// Undirected path problem
+const edges = [
+    ['i', 'j'],
+    ['k', 'i'],
+    ['m', 'k'],
+    ['k', 'l'],
+    ['o', 'n']
+]
+const undirectedPath = (edges, nodeA, nodeB) => {
+    const graph = buildGraph(edges);
+    console.log('built graph: ', graph)
+    return hasPath(graph, nodeA, nodeB, new Set())
+}
+const buildGraph = (edges) => {
+    const graph = {};
+    for(let edge of edges){
+        const [ a, b] = edge;
+        if(!(a in graph)) graph[a] = []
+        if(!(b in graph)) graph[b] = []
+        graph[a].push(b)
+        graph[b].push(a)
+    }
+    return graph
+}
+console.log('undirectedPath ',undirectedPath(edges, 'o', 'j'))
