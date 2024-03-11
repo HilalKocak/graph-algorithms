9b5953cae07cb6e3f2f4b589d8091b552047bfca
Hilal38 <hhilalkocak@gmail.com>
2024-03-11 06:31:19 +0300

    Add DFS stack

---
+++ graphalg.js
@@ -0,0 +1,21 @@
+const deptFirstPrint = (graph, source) => {
+    const stack = [ source ];
+
+    while ( stack.length > 0) {
+        const current = stack.pop();
+        console.log(current)
+        for(let neighbor of graph[current]){[
+            stack.push(neighbor)
+        ]}
+    }
+}
+const graph = {
+    a: ['b', 'c'],
+    b: ['d'],
+    c: ['e'],
+    d: ['f'],
+    e: [],
+    f: []
+}
+
+deptFirstPrint(graph, 'a');
