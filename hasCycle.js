4de0284b4b38f084e6419a375709533459990ec1
Hilal38 <hhilalkocak@gmail.com>
2024-04-07 09:49:37 +0300

    Implement has cycle

---
+++ hasCycle.js
@@ -0,0 +1,44 @@
+
+// Write a function, hasCycle, that takes in an object representing the adjacency list of a directed graph. The function should return a boolean indicating whether or not the graph contains a cycle.
+const hasCycle = (graph) => {
+    const visiting = new Set()
+    const visited = new Set()
+    for(let node in graph){
+      if(cycleDetect(graph, node, visiting, visited) === true){
+        return true
+      }
+    }
+    
+    return false
+  };
+  
+  const cycleDetect = (graph, node, visiting, visited) => {
+    if(visited.has(node)) return false
+    if(visiting.has(node)) return true // there is a cycle
+    
+    visiting.add(node)
+    
+    for(let neighbor of graph[node]){
+      if(cycleDetect(graph, neighbor, visiting, visited) === true){
+        return true
+      }
+    }
+    visiting.delete(node)
+    visited.add(node)
+    
+    return false
+  }
+
+  hasCycle({
+    a: ["b"],
+    b: ["c"],
+    c: ["a"],
+  }); // -> true
+
+
+  hasCycle({
+    a: ["b", "c"],
+    b: ["c"],
+    c: ["d"],
+    d: [],
+  }); // -> false
